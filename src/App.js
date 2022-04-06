c56ba64cbf0389383963594c6cd859702c43331f
juanrojas2140 <rojas_jc@hotmail.com>
2022-04-06 19:41:32 +0200

    characters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import { CharactersDetailPage } from './page/CharactersDetailPage';
+import { CharactersPage } from './page/CharactersPage';
 import { HomePage } from './page/HomePage';
 
 function App() {
   return (
     <div className="App">
-    <HomePage/>
+     <Router>
+       <Routes>
+        
+       <Route path='/characters' element={<CharactersPage />}/>
+       <Route path="/characters/:_id" element={<CharactersDetailPage />}/>
+       </Routes>
+       </Router>
     </div>
   );
 }
---
+++ src/page/CharactersDetailPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CharactersDetailPage = () => {
+  return (
+    <div>CharactersDetailPage</div>
+  )
+}
