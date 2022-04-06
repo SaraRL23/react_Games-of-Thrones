0338fd4ba64155f97f9ec25ad4398c7101ce4d07
SaraRL23 <saraanna03@hotmail.com>
2022-04-06 19:38:29 +0200

    homepage&navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+
+const Navbar = () => {
+    return (
+      <nav>
+        <ul className="text-menu">
+          <Link to="/characterspage">
+            <span className="personajes Text-Style-3" >PERSONAJES</span>
+          </Link>
+          <Link to="/casas">
+            <span className="casas Text-Style-3">CASAS</span>
+          </Link>
+          <Link to="/cronologia">
+            <span className="Cronologia Text-Style-3">CRONOLOGIA</span>
+          </Link>
+        </ul>
+        
+      </nav>
+    );
+  };
+  
+  export default Navbar;
---
+++ src/page/Cronologia.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Cronologia = () => {
+  return (
+    <div>Cronologia</div>
+  )
+}
