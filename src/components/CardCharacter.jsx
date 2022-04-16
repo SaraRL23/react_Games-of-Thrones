7ce25540f12e957c4491dfa237c6dca59ead390f
Juan Carlos Rojas Gamboa <rojas_jc@hotmail.com>
2022-04-16 16:19:10 +0200

    card

--- src/components/CardCharacter.jsx
+++ src/components/CardCharacter.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import { generatePath, Link } from 'react-router-dom'
-import "./card.css"
+import "./cardcharacter.css"
 
-export const CardCharacter = ({character}) => {
+export const Card = ({character}) => {
   return (
+    <>
+    {character && character.image &&
      <Link to={generatePath("/characters/:name", { name: character.name })}>
         <div className="gallery">
       <figure className='gallery_card' key={character._id} >
-     
+        
         <img className='gallery__img' src={character.image} alt={character.name} />
+
            <p className='gallery__text'>{character.name}</p>
       </figure>
       </div>
-     </Link>
+     </Link>}
+     </>
   )
 }
